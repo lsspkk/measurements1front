f79c425b5aa4e81c5167c4452724d7443faeb8a4
lvp <lsspkk@gmail.com>
2019-05-08 19:56:07 +0300

    new items with post method

--- src/Repository.js
+++ src/Repository.js
@@ -8,7 +8,7 @@ export default {
     //   { id: '1', name: 'Energy', type: 'slider', min: 0, max: 100 }
     // ]
     // return sampledata
-    return axios.get('/measure', {withCredentials:true}).then((res) => {
+    return axios.get('/measure', { withCredentials: true }).then((res) => {
       return res.data
     })
   },
@@ -24,17 +24,19 @@ export default {
       d.setDate(d.getDate() - 1)
       sampledata[i] = ({ 'timestamp': moment(d.toISOString(), 'YYYY-MM-DDTHH:mm:ss.SSSZ'), 'value': values[i] })
     }
-    //return data
-    return axios.get('/measurement/'+measureId, {withCredentials:true}).then((res) => {
-      //console.log(res.data)
+    // return data
+    return axios.get('/measurement/' + measureId, { withCredentials: true }).then((res) => {
+      // console.log(res.data)
       return res.data
     })
   },
 
-  putData(measureId, value, timestamp) {
-    return axios({ url: '/measurement/'+measureId, 
-      method: 'put', 
-        data: { value:value, timestamp:timestamp}, 
-      withCredentials:true})
+  postData (measureId, value, timestamp) {
+    return axios({
+      url: '/measurement/' + measureId,
+      method: 'post',
+      data: { value: value, timestamp: timestamp },
+      withCredentials: true
+    })
   }
 }
